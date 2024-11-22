fd9c7992ce4cf6c52dd166a36893e32a116424cb
Maurin Audenard <maurin@audenard.com>
2024-11-22 12:26:26 +0100

    feat(update): recup all shit

--- lua/maurin/core/keymaps.lua
+++ lua/maurin/core/keymaps.lua
@@ -1,50 +1,46 @@
 local map = vim.keymap.set
 
--- remap to name the space in wich-key
+-- NOTE : remap to name the space in wich-key
 map("n", "<leader>e", "", { desc = "File Explorer" })
 map("n", "<leader>f", "", { desc = "Fuzzy Finder" })
 map("n", "<leader>x", "", { desc = "Trouble #TODO" })
 
-map("n", "<left>",  '<cmd>echo "Use h to move !!"<CR>')
-map("n", "<right>", '<cmd>echo "Use l to move !!"<CR>')
-map("n", "<up>",    '<cmd>echo "Use k to move !!"<CR>')
-map('n', '<down>',  '<cmd>echo "Use j to move !!"<CR>')
-
--- remap Window
+-- NOTE : remap Window
 map("n", "<leader>a", "", { desc = "Windows"} )
+map("n", "<leader>at", ":terminal<CR>a",	{ desc = "Terminal" })
+map("n", "<leader>ao", ":only<CR>",					{ desc = "Only current" })
+map("n", "<leader>av", ":vsplit<CR>",				{ desc = "Vsplit" })
+map("n", "<leader>as", ":split<CR>",				{ desc = "Hsplit" })
 map("n", "<leader>ar", function()
-    if not pcall(vim.cmd, ":wq") then
-        vim.cmd(":wq!")
-    else
-        print "Save & escape !!!"
-    end
+	if not pcall(vim.cmd, ":wq") then
+		vim.cmd(":wq!")
+	else
+		print "Save & escape !!!"
+	end
 end,
-    {desc = "Save & close the current window" })
+	{desc = "Save & close the current window" })
 
 map("n", "<leader>aw", function()
-    if not pcall(vim.cmd, ":w") then
-        print "cannot save !"
-    else
-        print "File saved !"
-    end
+	if not pcall(vim.cmd, ":w") then
+		print "cannot save !"
+	else
+		print "File saved !"
+	end
 
 end,
-    {desc = "Save the current window" })
+	{desc = "Save the current window" })
 
 map("n", "<leader>ae", function()
-    if not pcall(vim.cmd, ":q") then
-        vim.cmd(":q!")
-    else
-        print "Escape !!!"
-    end
+	if not pcall(vim.cmd, ":q") then
+		vim.cmd(":q!")
+	else
+		print "Escape !!!"
+	end
 end,
-    {desc = "Save & close the current window" })
+	{desc = "Save & close the current window" })
 
-map("n", "<leader>at", ":vsplit<CR>:terminal<CR>a",  { desc = "Open a terminal" })
-map("n", "<leader>ao", ":only<CR>",                      { desc = "Only keep this window" })
-map("n", "<leader>av", ":vsplit<CR>",                    { desc = "Vertical split" })
-map("n", "<leader>as", ":split<CR>",                     { desc = "Horizontal split" })
-
+-- NOTE : navigation in multiple buffers
+map("n", "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", {desc = "Undotree"})
 map("n", "<leader>ah", "<C-w>h")
 map("n", "<leader>aj", "<C-w>j")
 map("n", "<leader>ak", "<C-w>k")
@@ -54,27 +50,39 @@ map("n", "<leader>aJ", "<C-w>J")
 map("n", "<leader>aK", "<C-w>K")
 map("n", "<leader>aL", "<C-w>L")
 
--- remap mouvements==
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
+-- NOTE : remap mouvements==
+map("n", "<C-d>", "<C-d> zz")
+map("n", "<C-u>", "<C-u> zz")
 
--- remap copy in clipboard
--- map("n", "<leader>y", '"+Y')
--- map("v", "<leader>y", '"+y')
--- map("n", "<leader>d", '"+D') 
--- map("v", "<leader>d", '"+D')
+-- NOTE : remap copy in clipboard
+map("n", "<leader>y", '"+Y')
+map("v", "<leader>y", '"+y')
+map("n", "<leader>d", '"+D')
+map("v", "<leader>d", '"+D')
 
--- remap déplacement de lignes
+-- NOTE : remap file Explorer
+map("n", "<leader>ee", "<cmd>Oil --float<CR>",{ desc = "Open" })
+map("n", "<leader>eh", "<cmd>bprev<CR>",	{ desc = "prev buffer" })
+map("n", "<leader>el", "<cmd>bnext<CR>",	{ desc = "next buffer" })
+map("n", "<leader>er", "<cmd>bdelete!<CR>",	{ desc = "next buffer" })
+
+-- NOTE : remap déplacement de lignes
 map("n", "H", "J")
-map("n", "<A-k>", ":m -2<CR>==",      { desc="Move line up"})
-map("n", "<A-j>", ":m +1<CR>==",      { desc="Move line down"})
-map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc="Move selection up"})
-map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc="Move selection up"})
+map("n", "<A-k>", ":m -2<CR>==",		{ desc="Move l up"})
+map("n", "<A-j>", ":m +1<CR>==",		{ desc="Move l down"})
+map("v", "<A-k>", ":m '<-2<CR>gv=gv",	{ desc="Move select up"})
+map("v", "<A-j>", ":m '>+1<CR>gv=gv",	{ desc="Move select up"})
 
--- remap pratique
+-- NOTE : remap pratique
 map("t", "<Esc>", "<C-\\><C-n>")
-map("n", "<leader>k", "zz",          { desc = "Center view" })
-map("n", "G", "Gzz",                 { desc = "Go full down"})
-map("n", "<C-h>", vim.lsp.buf.hover, { desc = "analyse under cursor"})
+map("n", "<leader>k", "zz",				{ desc = "Center view" })
+map("n", "G", "Gzz",					{ desc = "Go full down"})
+map("n", "<C-h>", vim.lsp.buf.hover,	{ desc = "analyse under cursor"})
 -- =a{ indentation entre les {}
-map("i", "<C-BS>", "<C-w>", { desc = "delete whole word"}) -- use ctrl-Backspace to delete the whole word in insert mode
+map("i", "<C-BS>", "<Esc>dbxa",				{ desc = "delete whole word"}) -- use ctrl-Backspace to delete the whole word in insert mode
+
+-- NOTE : remap folding
+map("n", "<leader>s", "",									{ desc = "Fold" })
+map("n", "<leader>st", "za",								{ desc = "toggle fold" })
+map("n", "<leader>s{", "f{zf%",								{ desc = "fold {}" })
+map("n", "<leader>sf", "<cmd>set foldmethod=indent<CR>",	{ desc = "fold over indent" })
